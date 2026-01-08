fa63b4d62ceb5931aa3d5a531794da5e017b2d93
Jishnu Shaji KK <jishnushajikk@gmail.com>
2026-01-08 17:47:14 +0530

    final

---
+++ cw5/src/main/java/com/example/cw5/Controller/homeController.java
@@ -0,0 +1,23 @@
+package com.example.cw5.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+@Controller
+public class homeController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+        }
+    
+    @GetMapping("/partials")
+    public String aboutus() {
+        return "index";
+    }
+    @GetMapping("/products")
+    public String products() {
+        return "products";
+    }
+}
+
+
