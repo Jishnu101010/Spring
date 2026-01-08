fe67e2a53fd6e70767ea515878f48775912227bc
Jishnu Shaji KK <jishnushajikk@gmail.com>
2026-01-08 15:11:58 +0530

    hii

--- cw4/src/main/java/com/example/cw4/Controller/ProductController.java
+++ cw4/src/main/java/com/example/cw4/Controller/ProductController.java
@@ -1,26 +1,32 @@
 package com.example.cw4.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import com.example.cw4.Models.Product;
-import java.util.List;
-import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.cw4.Model.Product;
+
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class ProductController {
 
     @GetMapping("/singleproduct")
-    public String getProduct(Model model) {
-    	Product product =new Product(101,"sugar",(float)55.5);
-    	   model.addAttribute("product", product);
-           return "product";
-       }
-    @GetMapping("/ProductList")
-    public String getProducts(Model, model) {
-    	List<Product> products=new ArrayList<>();
-    	products.add(new Product(101,"Sugar",(float)55.5));
-    	products.add(new Product(102,"Salt",(float)20.0));
-    	products.add(new Product(103,"Wheat Flour",(float) 38.75));
+    public String singleProduct(Model model) {
+        Product product = new Product(101, "Sugar", 55.5);
+        model.addAttribute("product", product);
+        return "single-product";
     }
 
+    @GetMapping("/productlist")
+    public String productList(Model model) {
+        List<Product> products = Arrays.asList(
+                new Product(101, "Sugar", 55.5),
+                new Product(102, "Salt", 20.0),
+                new Product(103, "Wheat Flour", 38.75)
+        );
+        model.addAttribute("products", products);
+        return "productlist";
+    }
 }
