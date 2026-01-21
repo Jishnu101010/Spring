b241545a3037373b2c8102530a0383f2116c980e
Jishnu Shaji KK <jishnushajikk@gmail.com>
2026-01-21 13:48:26 +0530

    HII

---
+++ cw12/src/test/java/com/example/cw12/Cw12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cw12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cw12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
